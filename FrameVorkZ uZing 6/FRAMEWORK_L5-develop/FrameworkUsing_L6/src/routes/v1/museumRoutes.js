02722918bdacdee2968341bd3101e7824f66589b
glokk18block <StefanenkovDimas@gmail.com>
2025-05-22 10:33:58 +0300

    Delete FrameVorkZ uZing 6/FRAMEWORK_L5-develop directory

--- FrameVorkZ uZing 6/FRAMEWORK_L5-develop/FrameworkUsing_L6/src/routes/v1/museumRoutes.js
+++
@@ -1,7 +0,0 @@
-const museumController = require('../../controllers/museumController');
-
-exports.museumRoutes = (server) => {
-  server.registerRoute('GET', '/api/v1/exhibits', museumController.getExhibits);
-  server.registerRoute('GET', '/api/v1/exhibits/:id', museumController.getExhibit);
-  server.registerRoute('POST', '/api/v1/exhibits', museumController.addExhibit);
-};
--- FrameVorkZ uZing 6/FRAMEWORK_L5-develop/README.md
+++
@@ -1,1 +0,0 @@
-# FrameworkUsing_L6
